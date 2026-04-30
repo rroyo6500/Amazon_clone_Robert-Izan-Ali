c63db25399883d6436138e19f468c34f55619c5d
r.royo <rroyo@proton.me>
2026-04-30 10:45:18 +0200

    Creating a NavigatorBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amazon_clone/components/nav_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: NavBar(),
       ),
     );
   }
