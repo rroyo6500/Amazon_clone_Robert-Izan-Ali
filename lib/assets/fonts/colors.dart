ca016344f08c17610a94aa5764641bf6a21f02ab
ali_cristaldo_b <ali_cristaldo@ceroca.cat>
2026-05-08 10:56:38 +0200

    colores gays

---
+++ lib/assets/fonts/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static const Color colorFondoLupa = Color(0xf8bc6a);
+  static const Color colorDeBarraMenu = Color(0x131921);
+  static const Color colorMenuDebajoDeBarra = Color(0x232f3e);
+
+}
