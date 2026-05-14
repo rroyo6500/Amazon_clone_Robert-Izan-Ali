0416bc14e8387e42b63a1ec4b5926be50b7146f0
Izan <izan_moreno@ceroca.cat>
2026-05-14 11:19:43 +0200

    .

--- lib/components/category.dart
+++ lib/components/category.dart
@@ -1,41 +1,25 @@
 import 'package:flutter/material.dart';
 
 class Category extends StatelessWidget {
-  const Category({super.key, required this.categoryName, required this.categoryImage});
+  const Category({super.key, required this.categoryName, required this.categoryImage,});
 
   final String categoryName;
   final Image categoryImage;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 200,
-          height: 200,
-          color: Colors.white,
-        
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 18),
-                  child: Text(
-                    categoryName
-                  ),
-                ),
-                SizedBox(height: 10,),
-                Center(
-                  child: categoryImage,
-                ),
-              ],
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Expanded(
+            child: SizedBox(
+              width: double.infinity,
+              child: categoryImage,
             ),
-          ),   
-        ),
-        
-      ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,79 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        
+        body: GridView.count(
+          crossAxisCount: 4,
+          crossAxisSpacing: 0,
+          mainAxisSpacing: 0,
+          children: [
+            Category(
+              categoryName: "categoryName",
+              categoryImage: Image.asset(
+                "lib/images/AmazonImagenOfertaTop.webp",
+                width: 273,
+                height: 273,
+              ),
+            ),
+            Category(
+              categoryName: "categoryName",
+              categoryImage: Image.asset(
+                "lib/images/AmazonImagenOfertasEnHogar.webp",
+                width: 273,
+                height: 273,
+              ),
+            ),
+            Category(
+              categoryName: "categoryName",
+              categoryImage: Image.asset(
+                "lib/images/AmazonImagenInformaticayAccesorios.webp",
+                width: 273,
+                height: 273,
+              ),
+            ),
+            Category(
+              categoryName: "categoryName",
+              categoryImage: Image.asset(
+                "lib/images/AmazonImagenPonteEnForma.webp",
+                width: 273,
+                height: 273,
+              ),
+            ),
+            Category(
+              categoryName: "categoryName",
+              categoryImage: Image.asset(
+                "lib/images/AmazonImagenImprescindiblesPartido.webp",
+                width: 273,
+                height: 273,
+              ),
+            ),
+            Category(
+              categoryName: "categoryName",
+              categoryImage: Image.asset(
+                "lib/images/AmazonImagenIdentificate.webp",
+                width: 273,
+                height: 273,
+              ),
+            ),
+            Category(
+              categoryName: "categoryName",
+              categoryImage: Image.asset(
+                "lib/images/AmazonImagenGrandesCelebracionesOferta.webp",
+                width: 273,
+                height: 273,
+              ),
+            ),
+            Category(
+              categoryName: "categoryName",
+              categoryImage: Image.asset(
+                "lib/images/AmazonImagenAhorraEnDispositivos.webp",
+                width: 273,
+                height: 273,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
